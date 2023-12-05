044e4b2fab705e964d35c9b114e643d970c0922c
kseb49 <kseb49@gmail.com>
2023-12-05 18:11:22 +0100

    add the delete link in the user list template and the csrf token for the delete action

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -14,6 +14,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[UniqueEntity("email")]
+#[UniqueEntity("username")]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
