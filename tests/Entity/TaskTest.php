f19b08e65b42d26c62d6a5c2d717cbe9e0872596
kseb49 <kseb49@gmail.com>
2023-12-27 17:54:37 +0100

    updates the phpunit.xml and wrote a new test

--- tests/Entity/TaskTest.php
+++ tests/Entity/TaskTest.php
@@ -4,6 +4,7 @@ namespace App\Tests\Entity;
 
 use App\Entity\Task;
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 class TaskTest extends TestCase
 {
@@ -11,6 +12,26 @@ class TaskTest extends TestCase
     {
         $task = new Task();
         $this->assertIsBool($task->isDone());
+    }
+
+
+    #[DataProvider('boolProvider')]
+    public function testToggle(bool $boolean)
+    {
+        $task = new Task();
+        $task->toggle($boolean);
+        $this->assertSame($boolean, $task->isDone());
+
+    }
+
+
+    public static function boolProvider(): array
+    {
+        return
+        [
+            [TRUE],
+            [FALSE],
+        ];
     }
 
 
